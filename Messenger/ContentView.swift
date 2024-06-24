dc83a604b1301356960166fb60e82d8aa3897952
Utsav Budathoki <utsavbudathoki12@gmail.com>
2024-06-24 23:30:26 +0930

    first commit

---
+++ Messenger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Messenger
+//
+//  Created by Utsav Budathoki on 24/6/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Utsav Budathoki on 24/6/2024.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
