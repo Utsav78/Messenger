9efa3013fe70ef76d9be2bf3284fba3ff913a7af
Utsav Budathoki <utsavbudathoki12@gmail.com>
2024-06-25 23:06:03 +0930

    Project setup

---
+++ Messenger/Core/Authentication/View/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Messenger
+//
+//  Created by Utsav Budathoki on 25/6/2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
