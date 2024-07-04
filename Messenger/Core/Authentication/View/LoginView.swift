f79050b80fca50725d2f6a485bf48caea39a55d6
Utsav Budathoki <utsavbudathoki12@gmail.com>
2024-07-04 22:17:24 +0930

    Project Setup

--- Messenger/Core/Authentication/View/LoginView.swift
+++ Messenger/Core/Authentication/View/LoginView.swift
@@ -9,7 +9,22 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            // logo image
+            
+            // textfield
+            
+            
+            // forgot button
+            
+            // login button
+            
+            
+            // facebook login
+            
+            // signup page
+            
+        }
     }
 }
 
