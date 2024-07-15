2c9db8f42654463b09c6364d388804d02b7521e6
Utsav Budathoki <utsavbudathoki12@gmail.com>
2024-07-15 23:45:43 +0930

    feat: Add InboxRowView & Integrate with InboxView

--- Messenger/Core/Inbox/InboxView.swift
+++ Messenger/Core/Inbox/InboxView.swift
@@ -12,6 +12,12 @@ struct InboxView: View {
         NavigationStack {
             ScrollView {
                 ActiveNowView()
+                
+                List(0 ... 10, id: \.self) { message in
+                    InboxRowView()
+                }
+                .listStyle(PlainListStyle())
+                .frame(height: UIScreen.main.bounds.height - 120)
             }
             .toolbar {
                 ToolbarItem(placement: .topBarLeading) {
