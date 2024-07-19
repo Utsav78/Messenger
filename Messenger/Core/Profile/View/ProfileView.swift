bf8e63e60d734822bf02d03763b71a165a9a1c55
Utsav Budathoki <utsavbudathoki12@gmail.com>
2024-07-19 23:41:21 +0930

    feat: Add ProfileView

--- Messenger/Core/Profile/View/ProfileView.swift
+++ Messenger/Core/Profile/View/ProfileView.swift
@@ -9,7 +9,50 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            VStack {
+                Image(systemName: "person.circle.fill")
+                    .resizable()
+                    .frame(width: 80, height: 80)
+                .foregroundStyle(Color(.systemGray6))
+                
+                Text("Bruce Wayne")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+            }
+            
+            List {
+                Section {
+                    ForEach(SettingsOptionsViewModel.allCases, id: \.self) { option in
+                        
+                        HStack {
+                            Image(systemName: option.imageName)
+                                .resizable()
+                                .frame(width: 24, height: 24)
+                                .foregroundStyle(option.imageBackgroundColor)
+                            
+                            Text(option.title)
+                                .font(.subheadline)
+                        }
+                        
+                    }
+                }
+                
+                Section {
+                    Button("Log out") {
+                        
+                    }
+                    
+                    Button("Delete Account") {
+                        
+                    }
+                }
+                .foregroundStyle(Color(.red))
+            }
+            
+            
+            
+        }
     }
 }
 
